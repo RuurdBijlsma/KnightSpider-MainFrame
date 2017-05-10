73dc621307016d66a75d26aaaa0b74dd6adfeaf2
Jorn van Wier <jornvanwier@gmail.com>
2017-05-10 11:41:37 +0200

    add debug rotation

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import time
 # Connect to the serial port
 serial_connection = Connection(port="/dev/serial0", baudrate=57600)
 
+dynamixel_id = 1
+
+serial_connection.goto(dynamixel_id, 0, speed=512, degrees=True)
+time.sleep(1)    # Wait 1 second
+
 leg = Leg(serial_connection)
 
 leg.move_to(Point3D(-180, 50, -60))
