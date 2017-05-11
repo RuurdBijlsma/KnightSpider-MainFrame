da370302ef6a05d390d425fdd994257e1e7ca61a
ruurd <ruurdbijlsma@gmail.com>
2017-05-11 15:46:08 +0200

    eem kommittte

--- test_ik.py
+++ test_ik.py
@@ -2,7 +2,7 @@ from actuator import Actuator
 from point import Point3D
 
 actuator = Actuator(['y', [80, 0., 0.], 'z', [80, 0., 0.], 'z', [120, 0., 0.]])
-angles = actuator.inverse_kinematics(Point3D(100, 100, 0))
-pos = actuator.forward_kinematics([20, 20, 20])
+angles = actuator.inverse_kinematics(Point3D(150, -80, 30))
+pos = actuator.forward_kinematics([15, 20, 20])
 print("Angles: ", angles)
 print("Position: ", pos)
