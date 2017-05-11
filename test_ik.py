8a54def3fce17a0e8aa173198df58c44bd032e58
ruurd <ruurdbijlsma@gmail.com>
2017-05-11 13:58:08 +0200

    Fixed negative values in inverse kinematics

--- test_ik.py
+++ test_ik.py
@@ -2,7 +2,7 @@ from actuator import Actuator
 from point import Point3D
 
 actuator = Actuator(['y', [80, 0., 0.], 'z', [80, 0., 0.], 'z', [120, 0., 0.]])
-angles = actuator.inverse_kinematics(Point3D(-30, 30, 0))
-pos = actuator.forward_kinematics([0, 0, 0])
+angles = actuator.inverse_kinematics(Point3D(100, 100, 0))
+pos = actuator.forward_kinematics([20, 20, 20])
 print("Angles: ", angles)
 print("Position: ", pos)
