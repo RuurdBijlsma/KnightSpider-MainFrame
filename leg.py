72b27f91cdd7657cbb40a16167ea5be043778375
ruurd <ruurdbijlsma@gmail.com>
2017-05-22 13:24:46 +0200

    walking animation added

--- leg.py
+++ leg.py
@@ -1,6 +1,22 @@
+import math
+
 from inverse_kinematics.actuator import Actuator
 from servo import Servo
 from point import Point3D
+
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
 
 
 class Leg(object):
@@ -8,17 +24,31 @@ class Leg(object):
     ALPHA_ID = 6
     BETA_ID = 16
 
-    def __init__(self, serial_connection):
+    def __init__(self, angle, serial_connection):
         self.serial_connection = serial_connection
         self.actuator = Actuator(['y', [77, 0., 0.], 'z', [80, 0., 0.], 'z', [115, 0., 0.]],
                                  max_angles=[150, 150, 150],
                                  min_angles=[-150, -150, -150])
         self.gamma = Servo(serial_connection, self.GAMMA_ID, offset_angle=0, min_angle=-60, max_angle=60)
-        self.alpha = Servo(serial_connection, self.ALPHA_ID, offset_angle=0, min_angle=-140, max_angle=70, flip_angles=True)
+        self.alpha = Servo(serial_connection, self.ALPHA_ID, offset_angle=0, min_angle=-140, max_angle=70,
+                           flip_angles=True)
         self.beta = Servo(serial_connection, self.BETA_ID, offset_angle=0, min_angle=-90, max_angle=140)
 
+        self.ground_height_offset = 0
+        self.angle = angle
+
+    def point_to_normalized(self, point):
+        origin = (0, 0)
+        tip_point = (point.x, point.z)
+        x, z = rotate(origin, tip_point, math.radians(self.angle))
+        return Point3D(x, point.y, z)
+
+    def move_to_normalized(self, point):
+        self.move_to(self.point_to_normalized(point))
+
     def move_to(self, point):
-        assert(point.y < 0)
+        point.y += self.ground_height_offset
+        assert (point.y < 0)
         angles = self.actuator.inverse_kinematics(point)
         # print("angles", point, angles)
         self.gamma.rotate_to(angles[0])
