1325befa5469545690e1513e8335f990a9967cf5
ruurd <ruurdbijlsma@gmail.com>
2017-05-22 15:28:15 +0200

    sequences update

--- sequences.py
+++ sequences.py
@@ -1,41 +1,44 @@
 from point import Point3D
 from stance import Stance
 
+step_length = 100
 step_delay = 0.3
+step_height = 50
+tip_distance = 150
 
 sequences = {
     'walking': [
         (Stance(
-            front_left_point=Point3D(150, 0, 50),
-            mid_left_point=Point3D(150, 0, -50),
-            back_left_point=Point3D(150, 0, 50),
-            front_right_point=Point3D(150, 0, -50),
-            mid_right_point=Point3D(150, 0, 50),
-            back_right_point=Point3D(150, 0, -50)
+            front_left_point=Point3D(tip_distance, 0, step_length / 2),
+            mid_left_point=Point3D(tip_distance, 0, -step_length / 2),
+            back_left_point=Point3D(tip_distance, 0, step_length / 2),
+            front_right_point=Point3D(tip_distance, 0, -step_length / 2),
+            mid_right_point=Point3D(tip_distance, 0, step_length / 2),
+            back_right_point=Point3D(tip_distance, 0, -step_length / 2)
         ), step_delay),
         (Stance(
-            front_left_point=Point3D(150, 0, 0),
-            mid_left_point=Point3D(150, 20, 0),
-            back_left_point=Point3D(150, 0, 0),
-            front_right_point=Point3D(150, 20, 0),
-            mid_right_point=Point3D(150, 20, 0),
-            back_right_point=Point3D(150, 20, 0)
+            front_left_point=Point3D(tip_distance, 0, 0),
+            mid_left_point=Point3D(tip_distance, step_height, 0),
+            back_left_point=Point3D(tip_distance, 0, 0),
+            front_right_point=Point3D(tip_distance, step_height, 0),
+            mid_right_point=Point3D(tip_distance, 0, 0),
+            back_right_point=Point3D(tip_distance, step_height, 0)
         ), step_delay),
         (Stance(
-            front_left_point=Point3D(150, 0, -50),
-            mid_left_point=Point3D(150, 0, 50),
-            back_left_point=Point3D(150, 0, -50),
-            front_right_point=Point3D(150, 0, 50),
-            mid_right_point=Point3D(150, 0, -50),
-            back_right_point=Point3D(150, 0, 50)
+            front_left_point=Point3D(tip_distance, 0, -step_length / 2),
+            mid_left_point=Point3D(tip_distance, 0, step_length / 2),
+            back_left_point=Point3D(tip_distance, 0, -step_length / 2),
+            front_right_point=Point3D(tip_distance, 0, step_length / 2),
+            mid_right_point=Point3D(tip_distance, 0, -step_length / 2),
+            back_right_point=Point3D(tip_distance, 0, step_length / 2)
         ), step_delay),
         (Stance(
-            front_left_point=Point3D(150, 20, 0),
-            mid_left_point=Point3D(150, 0, 0),
-            back_left_point=Point3D(150, 20, 0),
-            front_right_point=Point3D(150, 0, 0),
-            mid_right_point=Point3D(150, 20, 0),
-            back_right_point=Point3D(150, 0, 0)
+            front_left_point=Point3D(tip_distance, step_height, 0),
+            mid_left_point=Point3D(tip_distance, 0, 0),
+            back_left_point=Point3D(tip_distance, step_height, 0),
+            front_right_point=Point3D(tip_distance, 0, 0),
+            mid_right_point=Point3D(tip_distance, step_height, 0),
+            back_right_point=Point3D(tip_distance, 0, 0)
         ), step_delay),
     ]
 }
