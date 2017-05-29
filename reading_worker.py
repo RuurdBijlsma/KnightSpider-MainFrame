fc38b2a97a6f757ef2b72c567533db201412a19a
ruurd <ruurdbijlsma@gmail.com>
2017-05-29 12:03:32 +0200

    get readings added to worker

--- reading_worker.py
+++ reading_worker.py
@@ -3,18 +3,25 @@
 import threading
 import time
 
+import utils
 
-class Worker(threading.Thread):
-    def __init__(self, frequency):
+
+class ReadingUpdater(threading.Thread):
+    def __init__(self, frequency, spider):
         self.delay = 1 / frequency
+        self.spider = spider
         threading.Thread.__init__(self)
 
     def run(self):
         print("Starting worker")
-        self.refresh_readings()
-        print("Exiting worker")
-
-    def refresh_readings(self):
         while True:
             time.sleep(self.delay)
-            print("Updating readings")
+            self.refresh_readings()
+
+    def refresh_readings(self):
+        print("Updating readings")
+        print("Usage:", utils.get_cpu_usage())
+        print("Temp:", utils.get_cpu_temp())
+        self.temperature = utils.get_cpu_temp()
+        self.cpu_usage = utils.get_cpu_usage()
+        self.servo_info = self.spider.get_readings()
--- test/test_readings.py
+++ test/test_readings.py
@@ -1,4 +1,24 @@
-from reading_worker import Worker
+from leg import Leg
+from reading_worker import ReadingUpdater
+from spider import Spider
 
-background_thread = Worker(frequency=2)
+legs = [
+    Leg(30, 11, 12, 13),
+    Leg(0, 21, 22, 23),
+    Leg(-30, 31, 32, 33),
+    Leg(30, 41, 42, 43),
+    Leg(-30, 31, 32, 33),
+    Leg(30, 41, 42, 43),
+]
+
+spider = Spider(
+    front_right_leg=legs[0],
+    mid_right_leg=legs[1],
+    back_right_leg=legs[2],
+    front_left_leg=legs[3],
+    mid_left_leg=legs[4],
+    back_left_leg=legs[5],
+)
+
+background_thread = ReadingUpdater(frequency=2, spider=spider)
 background_thread.start()
