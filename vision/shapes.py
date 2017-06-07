0f3a92935e71c5ef4e58b4bf41449bd2bd229d56
Frank Keuning <Frank_keuning@hotmail.com>
2017-06-07 13:45:20 +0200

    added arguments
    het script werkt nu met en zonder arguments

--- vision/shapes.py
+++ vision/shapes.py
@@ -6,32 +6,26 @@ import sys
 print(sys.argv[0])
 targetImg = cv2.imread('images/club.jpg',0)
 
-if sys.argv[1] == "diamond":
-    targetImg = cv2.imread('images/ruiten.png',0)
-    print("diamond selected")
-elif sys.argv[1] == "club":
-    targetImg = cv2.imread('images/club.jpg',0)
-    print("club selected")
-elif sys.argv[1] == "spade":
-    targetImg = cv2.imread('images/spade.jpg',0)
-    print("spade selected")
+if len(sys.argv) > 1:
+    if sys.argv[1] == "diamond":
+        targetImg = cv2.imread('images/ruiten.png',0)
+        print("diamond selected")
+    elif sys.argv[1] == "club":
+        targetImg = cv2.imread('images/club.jpg',0)
+        print("club selected")
+    elif sys.argv[1] == "spade":
+        targetImg = cv2.imread('images/spade.jpg',0)
+        print("spade selected")
+    else:
+        targetImg = cv2.imread('images/hart2.jpg',0)
+        print("hart selected")
 else:
     targetImg = cv2.imread('images/hart2.jpg',0)
     print("hart selected")
 
 
-#targetImg = cv2.imread('images/club.jpg',0)
-#targetImg = cv2.imread('images/spade.jpg',0)
-#targetImg = cv2.imread('images/ruiten.png',0)
-
-#img2 = cv2.imread('23.jpg',0)
 
 cap = cv2.VideoCapture(0)
-
-
-#_, thresh = cv2.threshold(targetImg, 127, 255,0)
-#_,contours,_ = cv2.findContours(thresh,2,1)
-#cnt1 = contours[1]
 
 MIN_AREA = 5
 MAX_SIMILARITY_VALUE = 0.1
