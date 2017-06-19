4b3383b77c28b27b6e60c392efe4935a50d216d5
Jorn van Wier <jornvanwier@gmail.com>
2017-06-19 12:32:41 +0200

    add gyro test

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import time
 
 import ax12_serial
 import utils
+from gyroscoop import Gyroscoop
 from spider import Spider
 
 print("temp", utils.get_cpu_temp())
+g = Gyroscoop()
+print("angle", g.get_x_rotation(g.read_gyro()))
 
 ax12_serial.init()
 spider = Spider()
